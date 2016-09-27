7d1a7633cbd194fb54396afd56dea3861ed3f1cc
Lukasz Lechowicz <llechowicz@gmail.com>
2016-09-27 20:22:15 +0000

    More tweaks just to make it work...

--- nfc.py
+++ nfc.py
@@ -26,6 +26,8 @@ players = list()
 game_on = False
 game_start_time = None
 last_card = None
+first_registered = None
+
 PLAYER_2_WAIT_TIME = 5
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
@@ -36,7 +38,8 @@ while continue_reading:
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
-        print "Card detected"
+        # print "Card detected"
+        pass
     
     # Get the UID of the card
     status, uid = MIFAREReader.MFRC522_Anticoll()
@@ -45,7 +48,6 @@ while continue_reading:
     if status == MIFAREReader.MI_OK:
 
         # print "UID: ", '-'.join(uid)
-        print uid
 
         if uid not in players and len(players) < 2:  # do not add players if there's already two
             players.append(uid)
@@ -68,26 +70,35 @@ while continue_reading:
         #
         # print ""
 
-    # Start the timer (PLAYER_2_WAIT_TIME) if the 1st player registers
-    if len(players) == 1:
-        first_registered = datetime.now()
-        print "Player #1 registered. Waiting for players 2..."
+        # Start the timer (PLAYER_2_WAIT_TIME) if the 1st player registers
+        if len(players) == 1:
+            first_registered = datetime.now()
+            print "Player #1 registered. Waiting for players 2..."
 
-    # Reset the players list if 2nd player haven't registered
-    # within the PLAYER_2_WAIT_TIME seconds after player #1 registered
-    if (datetime.now() - first_registered).seconds > PLAYER_2_WAIT_TIME and len(players) < 2:
-        players = list()
-        print "Player #2 did not register on time (%d seconds passed). Resetting..." % PLAYER_2_WAIT_TIME
+        # Reset the players list if 2nd player haven't registered
+        # within the PLAYER_2_WAIT_TIME seconds after player #1 registered
+        if first_registered and (datetime.now() - first_registered).seconds > PLAYER_2_WAIT_TIME and len(players) < 2:
+            players = list()
+            print "Player #2 did not register on time (%d seconds passed). Resetting..." % PLAYER_2_WAIT_TIME
 
-    # Begin the game (game_on) and start the game time (time_elapsed)
-    # if both players registered within the given time
-    if len(players) == 2 and game_on is False:
-        game_start_time = datetime.now()
-        game_on = True
-        print "Both players registered. Game begins..."
+        # Begin the game (game_on) and start the game time (time_elapsed)
+        # if both players registered within the given time
+        if len(players) == 2 and game_on is False:
+            game_start_time = datetime.now()
+            game_on = True
+            print "Both players registered. Game begins..."
 
-    time_elapsed = (datetime.now() - game_start_time).seconds
-    if PLAYER_2_WAIT_TIME * 2 < time_elapsed < 60*60*30:
-        print "Player #%d won" % players.index(uid) + 1
-        game_on = False
-        players = list()
+        time_elapsed = 0
+        if game_start_time is not None:
+            time_elapsed = (datetime.now() - game_start_time).seconds
+
+        if PLAYER_2_WAIT_TIME < time_elapsed < 60*60*30:
+            print "Player #{} won".format(players.index(uid) + 1)
+            print "Game took {} minute(s) and {} second(s)".format(time_elapsed/60, time_elapsed%60)
+            game_on = False
+            players = list()
+            first_registered = None
+            game_start_time = None
+            print ""
+            print "Waiting for new players..."
+            print ""
