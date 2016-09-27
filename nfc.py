e20a90dc652588d12dbdf6c447c4ae28b52fa11e
Łukasz Lechowicz <llechowicz@gmail.com>
2016-09-27 20:38:56 +0100

    Pool game flow. WIP

--- nfc.py
+++ nfc.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-
 import RPi.GPIO as GPIO
 import MFRC522
 import signal
+from datetime import datetime
 
 continue_reading = True
+
 
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal, frame):
@@ -20,6 +21,12 @@ signal.signal(signal.SIGINT, end_read)
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 
+# Some Pool vars
+players = list()
+game_on = False
+game_start_time = None
+last_card = None
+PLAYER_2_WAIT_TIME = 5
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
@@ -37,26 +44,50 @@ while continue_reading:
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
 
-        # Print UID
         # print "UID: ", '-'.join(uid)
         print uid
 
-        # This is the default key for authentication
-        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-        
-        # Select the scanned tag
-        MIFAREReader.MFRC522_SelectTag(uid)
+        if uid not in players and len(players) < 2:  # do not add players if there's already two
+            players.append(uid)
 
-        # Authenticate
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+        # # This is the default key for authentication
+        # key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+        #
+        # # Select the scanned tag
+        # MIFAREReader.MFRC522_SelectTag(uid)
+        #
+        # # Authenticate
+        # status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+        #
+        # # Check if authenticated
+        # if status == MIFAREReader.MI_OK:
+        #     MIFAREReader.MFRC522_Read(8)
+        #     MIFAREReader.MFRC522_StopCrypto1()
+        # else:
+        #     print "Authentication error"
+        #
+        # print ""
 
-        # Check if authenticated
-        if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-        else:
-            print "Authentication error"
-        print ""
+    # Start the timer (PLAYER_2_WAIT_TIME) if the 1st player registers
+    if len(players) == 1:
+        first_registered = datetime.now()
+        print "Player #1 registered. Waiting for players 2..."
 
+    # Reset the players list if 2nd player haven't registered
+    # within the PLAYER_2_WAIT_TIME seconds after player #1 registered
+    if (datetime.now() - first_registered).seconds > PLAYER_2_WAIT_TIME and len(players) < 2:
+        players = list()
+        print "Player #2 did not register on time (%d seconds passed). Resetting..." % PLAYER_2_WAIT_TIME
 
-    
+    # Begin the game (game_on) and start the game time (time_elapsed)
+    # if both players registered within the given time
+    if len(players) == 2 and game_on is False:
+        game_start_time = datetime.now()
+        game_on = True
+        print "Both players registered. Game begins..."
+
+    time_elapsed = (datetime.now() - game_start_time).seconds
+    if PLAYER_2_WAIT_TIME * 2 < time_elapsed < 60*60*30:
+        print "Player #%d won" % players.index(uid) + 1
+        game_on = False
+        players = list()
