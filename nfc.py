a5f357063aeffe7ef0be25f580c4d05d3271b3dc
Łukasz Lechowicz <llechowicz@gmail.com>
2016-09-28 21:45:37 +0100

    Rearranging things a bit.

--- nfc.py
+++ nfc.py
@@ -24,13 +24,20 @@ MIFAREReader = MFRC522.MFRC522()
 # Some Pool vars
 players = list()
 game_on = False
-game_start_time = None
-last_card = None
-first_registered = None
+game_timer = None
+player_1_reg_time = None
+player_2_reg_time = None
+card_loops = 0
 
-PLAYER_2_WAIT_TIME = 5
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+def reset_game():
+    players = list()
+    game_on = False
+    player_1_reg_time = None
+    player_2_reg_time = None
+    card_loops = 0
+
+
 while continue_reading:
     
     # Scan for cards    
@@ -44,61 +51,43 @@ while continue_reading:
     # Get the UID of the card
     status, uid = MIFAREReader.MFRC522_Anticoll()
 
+    if status != MIFAREReader.MI_OK:
+        card_loops = 0
+
     # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
+    else:
+        card_loops += 1
 
-        # print "UID: ", '-'.join(uid)
-
-        if uid not in players and len(players) < 2:  # do not add players if there's already two
+        players_count = len(players)
+        if uid not in players and players_count < 2:
             players.append(uid)
+            if players_count == 1:
+                print "Player #1 registered."
+                player_1_reg_time = datetime.now()
+            if players_count == 2:
+                print "Player #2 registered."
+                player_2_reg_time = datetime.now()
 
-        # # This is the default key for authentication
-        # key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-        #
-        # # Select the scanned tag
-        # MIFAREReader.MFRC522_SelectTag(uid)
-        #
-        # # Authenticate
-        # status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-        #
-        # # Check if authenticated
-        # if status == MIFAREReader.MI_OK:
-        #     MIFAREReader.MFRC522_Read(8)
-        #     MIFAREReader.MFRC522_StopCrypto1()
-        # else:
-        #     print "Authentication error"
-        #
-        # print ""
+        if players_count < 2 and (datetime.now() - player_1_reg_time).seconds > 5:
+            print "Players removed. Register both players faster."
+            reset_game()
 
-        # Start the timer (PLAYER_2_WAIT_TIME) if the 1st player registers
-        if len(players) == 1:
-            first_registered = datetime.now()
-            print "Player #1 registered. Waiting for players 2..."
+        # Never pass this point if players aren't registered
+        if players_count < 2:
+            continue
 
-        # Reset the players list if 2nd player haven't registered
-        # within the PLAYER_2_WAIT_TIME seconds after player #1 registered
-        if first_registered and (datetime.now() - first_registered).seconds > PLAYER_2_WAIT_TIME and len(players) < 2:
-            players = list()
-            print "Player #2 did not register on time (%d seconds passed). Resetting..." % PLAYER_2_WAIT_TIME
-
-        # Begin the game (game_on) and start the game time (time_elapsed)
-        # if both players registered within the given time
-        if len(players) == 2 and game_on is False:
-            game_start_time = datetime.now()
-            game_on = True
-            print "Both players registered. Game begins..."
+        game_timer = datetime.now()
+        game_on = True
+        print "Both players registered. Game begins..."
 
         time_elapsed = 0
-        if game_start_time is not None:
-            time_elapsed = (datetime.now() - game_start_time).seconds
+        if game_timer is not None:
+            time_elapsed = (datetime.now() - game_timer).seconds
 
-        if PLAYER_2_WAIT_TIME < time_elapsed < 60*60*30:
+        if 5 < time_elapsed < 60*60*30:
             print "Player #{} won".format(players.index(uid) + 1)
-            print "Game took {} minute(s) and {} second(s)".format(time_elapsed/60, time_elapsed%60)
-            game_on = False
-            players = list()
-            first_registered = None
-            game_start_time = None
+            print "Game took {} minute(s) and {} second(s)".format(time_elapsed / 60, time_elapsed % 60)
+            reset_game()
             print ""
             print "Waiting for new players..."
             print ""
